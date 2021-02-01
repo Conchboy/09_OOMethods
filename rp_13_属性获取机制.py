511905c40293d68b7ccaf23ad1569a0d14498029
conchboy <panchaocn@hotmail.com>
2021-02-01 23:02:46 +0800

    update from Laptop

--- rp_13_属性获取机制.py
+++ rp_13_属性获取机制.py
@@ -2,12 +2,14 @@ class Tool(object):
 
     # 使用赋值语句定义类属性,记录所有工具对象的数量
     count = 0
+    list = []
 
     def __init__(self, name):
         self.name = name
 
         # 让属性的值+1; 记录和类相关的特征
         Tool.count += 1
+        list.append(name)
 
 
 # 1. 创建工具对象
@@ -17,5 +19,5 @@ tool3 = Tool("水桶")
 tool4 = Tool("类属性")
 
 # 2. 输出工具对象的总数
-print(Tool.count)
-print(tool2.count)
+print("工具对象总数 %d" % tool1.count)
+print(tool2.list)
