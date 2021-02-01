a995947cfa34d635a6e3f737f08df3fc3c8c8dd8
conchboy <panchaocn@hotmail.com>
2021-02-01 22:55:08 +0800

    update from Laptop

---
+++ rp_12_类属性.py
@@ -0,0 +1,20 @@
+class Tool(object):
+
+    # 使用赋值语句定义类属性,记录所有工具对象的数量
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+
+        # 让属性的值+1; 记录和类相关的特征
+        Tool.count += 1
+
+
+# 1. 创建工具对象
+tool1 = Tool("斧头")
+tool2 = Tool("榔头")
+tool3 = Tool("水桶")
+tool4 = Tool("类属性")
+
+# 2. 输出工具对象的总数
+print(Tool.count)
